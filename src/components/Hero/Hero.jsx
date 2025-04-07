53ebe95e918dde4319140cca5a9af70d1be7b565
MH-Shihab <mhsshihab7@gmail.com>
2025-04-07 10:57:25 +0600

    complete full poject

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Shihab</h1>
+        <p className={styles.description}>
+        I'm a full stack developer with 2 years of experience in building dynamic and scalable web applications using the MERN stack and Laravel.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/man-hero.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
